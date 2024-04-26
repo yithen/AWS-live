14111bef1ed2c483946b1f1bf7456df7b768d912
yithen <110660720+yithen@users.noreply.github.com>
2024-04-26 21:19:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cxmym02as3td.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aacs3523"
+customdb = "employee"
+custombucket = "tanyh-employee"
+customregion = "us-east-1"
+
